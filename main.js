e209b2ef78cfbad683de294a2302f4fd9a252151
MarcosMartinsDeLima <0timetraveler@gmail.com>
2025-03-20 21:33:58 -0300

    feat: atividade

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
-var lista = ['Abner', 'Lucas', 'Santos'];
-console.log (lista)
+// var lista = ['Abner', 'Lucas', 'Santos'];
+// console.log (lista)
 
-console.log(lista[2]);
-console.log(lista[0]);
+// console.log(lista[2]);
+// console.log(lista[0]);
 
-lista.push('Alvarenga');
-console.log(lista);
+// lista.push('Alvarenga');
+// console.log(lista);
 
-lista.pop();
-console.log(lista.length)
+// lista.pop();
+// console.log(lista.length)
+
+
+function Alunos(nome,nota){
+    this.nome = nome
+    this.nota = nota
+}
+
+let listDeAlunos = []
+
+let abner = new Alunos("Abner",10)
+listDeAlunos.push(abner)
+
+let marcos = new Alunos("Marcos",8)
+listDeAlunos.push(marcos)
+
+listDeAlunos.forEach(aluno =>{
+    console.log(`Aluno: ${aluno.nome} , nota: ${aluno.nota}`)
+})
