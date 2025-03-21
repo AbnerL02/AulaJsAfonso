97da9bb8e8b7754b792f174ecbbf8165c6695e75
MarcosMartinsDeLima <0timetraveler@gmail.com>
2025-03-20 21:24:42 -0300

    feat: manupulacao de array

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-function Carro(valor,cor,ano,modelo,popular){
-    this.valor = valor
-    this.cor = cor
-    this.ano = ano
-    this.modelo = modelo
-    this.popular = popular
-}
+var lista = ['Abner', 'Lucas', 'Santos'];
+console.log (lista)
 
-let Porsche = new Carro (150000,"cinza",2023,"911 Turbo S", false)
-console.log(Porsche)
+console.log(lista[2]);
+console.log(lista[0]);
+
+lista.push('Alvarenga');
+console.log(lista);
+
+lista.pop();
+console.log(lista.length)
